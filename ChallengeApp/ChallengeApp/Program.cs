744b9defb2fa99cf9d00290ca680284fe7bb2b76
maciejleszcz95 <maciejleszczynski@icloud.com>
2024-04-14 18:50:40 +0200

    Zadanie domowe nr 2

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Maciej"; // imie 
+char sex = 'm'; // m - mezczyzna, f - kobieta
+ushort age = 29;
+
+// pierwsza instrukcja warunkowa
+if (sex == 'f' && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+// druga instrukcja warunkowa
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+// trzecia instrukcja warunkowa
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+// czwarty przypadek
+else
+{
+    ;
+    // program nic nie wyswietla (pusta instrukcja)
+}
