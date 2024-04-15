9bfdaeaadd175c57e4ad944c4c452526f3ebef8d
maciejleszcz95 <maciejleszczynski@icloud.com>
2024-04-15 18:24:59 +0200

    Zadanie Dodatkowe Odwrocenie liczby

---
+++ ZadanieDodatkowe3/ZadanieDodatkowe3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Program ktory odwraca liczbe np. 1344 => 4431
+
+int liczba = 1344;
+int liczba2 = 0;
+
+char[] tablicaCyfr = liczba.ToString().ToArray() as char[];
+tablicaCyfr = tablicaCyfr.Reverse().ToArray();
+liczba2 = int.Parse(tablicaCyfr);
+
+Console.WriteLine($"Liczba wejsciowa: {liczba}\nLiczba odwrocona: {liczba2}");
