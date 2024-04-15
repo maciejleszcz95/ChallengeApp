ff2eab8cab3e6903c88cb2e3070df27a2770478a
maciejleszcz95 <maciejleszczynski@icloud.com>
2024-04-15 18:57:26 +0200

    Zadanie Dodatkowe Sortowanie tablicy liczb rosnaco

---
+++ ZadanieDodatkowe4/ZadanieDodatkowe4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Program sortujacy liczby w tablicy
+
+int[] tablica = { 7, 5, 1, 9, 4, 2, 3, 6, 8 };
+int bufor = 0;
+
+// wyswietlenie tablicy nieposortowanej
+Console.WriteLine("Tablica nieposortowana:");
+foreach(int liczba in tablica)
+{
+    Console.Write(liczba + " ");
+}
+
+// sortowanie tablicy liczb rosnaco
+for (int i = 0; i < tablica.Length; i++)
+{
+    for (int j = i + 1; j < tablica.Length; j++)
+    {
+        if(tablica[j] < tablica[i])
+        {
+            bufor = tablica[i];
+            tablica[i] = tablica[j];
+            tablica[j] = bufor;
+        }
+    }
+}
+
+// wyswietlenie tablicy posortowanej
+Console.WriteLine("\nTablica posortowana:");
+foreach (int liczba in tablica)
+{
+    Console.Write(liczba + " ");
+}
