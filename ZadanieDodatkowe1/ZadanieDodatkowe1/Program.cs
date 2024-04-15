c70a910870128e95242fa9b907fd59ebaddd636d
maciejleszcz95 <maciejleszczynski@icloud.com>
2024-04-15 17:35:41 +0200

    Zadanie Dodatkowe Alfabet

---
+++ ZadanieDodatkowe1/ZadanieDodatkowe1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Program ktory wyswietli caly alfabet
+char poczatek = 'a';
+char koniec = 'z';
+char znak = poczatek;
+
+
+while (true)
+{
+    Console.Write(znak);
+    znak++;
+    if (znak > koniec)
+    {
+        break;
+    }
+}
+
